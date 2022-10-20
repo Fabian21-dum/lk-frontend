c6bab2b09a7193bca22bfc3005634e999e0bc63e
Faidil <faidilfamozzy@gmail.com>
2022-10-20 17:48:31 +0800

    add some section in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,13 +6,16 @@ import Button from '../components/Button';
 import LogoSTMIKWICIDA from '../assets/Logo_STMIK_WICIDA.png';
 import LogoLOKreatif from '../assets/Logo_LO_KREATIF.png';
 import LogoAptisi from '../assets/Logo_APTISI.png';
+import VectorImage1 from '../assets/Vector1.svg';
+import VectorImage2 from '../assets/Vector2.svg';
+import ZigZag from '../assets/ZigZag.svg';
 
 export default function HomePage() {
   return (
     <HomeLayout>
-      <header className='h-[800px] bg-gradient-to-b from-cyan-400 px-4 pt-16'>
-        <div className='item container flex items-center justify-between'>
-          <section className='w-1/2 max-w-md'>
+      <header className='h-[800px] bg-gradient-to-b from-cyan-300 px-4 pt-16'>
+        <div className='container flex items-center justify-between'>
+          <div className='w-1/2 max-w-md'>
             <h2 className='text-6xl font-bold'>Tracking Belajar mu! Bersama Kami</h2>
             <p className=' py-5 font-semibold'>
               Belajar mudah dengan adanya fitur - fitur yang dapat membantu-mu memahami bagaimana progress belajar mu
@@ -21,10 +24,10 @@ export default function HomePage() {
             <Link to='/login'>
               <Button>Get Started</Button>
             </Link>
-          </section>
-          <section className='w-1/2'>
+          </div>
+          <div className='w-1/2'>
             <img className='mx-auto h-[564px]' src={HeroImage} alt='hero-img' />
-          </section>
+          </div>
         </div>
       </header>
       {/* partnership section */}
@@ -34,6 +37,34 @@ export default function HomePage() {
           <img className='h-[96px]' src={LogoSTMIKWICIDA} alt='Logo STMIK WICIDA' />
           <img className='h-[96px]' src={LogoLOKreatif} alt='Logo STMIK WICIDA' />
           <img className='h-[96px]' src={LogoAptisi} alt='Logo STMIK WICIDA' />
+        </div>
+      </section>
+      <section className='bg bg-gradient-to-t from-cyan-300 pt-14 pb-6'>
+        <div className='container flex items-center justify-evenly'>
+          <div className='w-1/2'>
+            <img src={VectorImage1} alt='vector-1' />
+          </div>
+          <div className='w-1/2 max-w-lg'>
+            <h2 className='pb-5 text-6xl font-bold'>Liat Keseharian Anda Dalam Belajar.</h2>
+            <p className='font-semibold'>
+              Proses belajar yang dapat di lacak adalah proses yang tentu akan membuat bagaimana pandagan pelajar
+              sangatlah menarik dan menjadi sebuah kebangaan tertentu dalam melihat peroses belajarnya.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className='bg-cyan-300 py-4'>
+        <img src={ZigZag} alt='zigzag-vector' />
+      </section>
+      <section className='bg-gradient-to-b from-cyan-300 py-8'>
+        <div className='container'>
+          <h1 className='pt-8 text-center text-3xl font-bold'>Apa saja manfaat Tracking proses belajar? </h1>
+          <div className='align-center flex'>
+            <div className='w-2/3'></div>
+            <div className='w-1/3'>
+              <img src={VectorImage2} alt='' />
+            </div>
+          </div>
         </div>
       </section>
     </HomeLayout>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,24 +5,18 @@ import { Link } from 'react-router-dom';
 // import { Navigate } from 'react-router-dom';
 
 export default function LoginPage() {
-  {
-    /* jika isLogin true redirect ke  */
-  }
-  {
-    /* <Navigate to='/dashboard' replace={true} /> */
-  }
   return (
     <RootLayout>
       <div className='flex h-screen w-full items-center justify-center bg-[#ABFBFE]'>
-        <div className='h-fit w-[80%] rounded-xl border border-none bg-white p-4 md:flex md:w-fit md:flex-row-reverse md:gap-16 md:p-24'>
+        <div className='h-fit w-[80%] rounded-xl bg-white p-4 md:flex md:w-fit md:flex-row-reverse md:gap-16 md:p-24'>
           <div className='mb-4 text-center  md:hidden'>
             <h1 className='text-2xl font-extrabold'>SELAMAT DATANG</h1>
             <h3 className='text-sm font-medium'>Selamat Datang Kembali Sob.</h3>
           </div>
-          <div className='mr-auto  ml-auto  '>
-            <img src={loginLogo} alt='login' className='mr-auto ml-auto h-48 md:h-80 ' />
+          <div className='mx-auto'>
+            <img src={loginLogo} alt='login' className='mx-auto h-48 md:h-80 ' />
           </div>
-          <div className='mr-auto ml-auto flex flex-col items-center justify-center'>
+          <div className='mx-auto flex flex-col items-center justify-center'>
             <div className='mb-4 hidden text-center md:block'>
               <h1 className='text-2xl font-extrabold lg:text-4xl'>SELAMAT DATANG</h1>
               <h3 className='text-md lg:text-1xl font-medium'>Selamat Datang Kembali Sob.</h3>
@@ -55,7 +49,7 @@ export default function LoginPage() {
                 </div>
                 <button
                   type='Submit'
-                  className='w-full rounded-lg border border-black bg-[#BDF7F9] py-2.5  text-center text-sm font-bold text-black '
+                  className='w-full rounded-lg border border-black bg-[#BDF7F9] py-2.5  text-center text-sm font-bold text-black'
                 >
                   Login
                 </button>
